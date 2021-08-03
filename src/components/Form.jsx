823a0eafcd7c71d06a7646f674fbd02ee774797f
ryanp <ryanpanimation@gmail.com>
2021-08-03 14:28:05 -0500

    add ratings

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,18 @@ import { useHistory } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import Button from '@material-ui/core/Button';
 
-const names = ['Aida', 'Azzy', 'Cara', 'Jackob', 'Rebecca', 'Ryan'];
+const names = ['Aida', 'Azzy', 'Cara', 'Jakob', 'Rebecca', 'Ryan'];
+const EMPTY_FORM = { name: '', rating: '' };
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,15 +24,32 @@ const useStyles = makeStyles((theme) => ({
     minWidth: '65vw',
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
+    margin: theme.spacing(2, 0),
   },
   selection: {
     '&:hover': {
       backgroundColor: '#badcd0',
     },
   },
+  radioLabel: {
+    textAlign: 'left',
+    margin: theme.spacing(2, 0, 1),
+  },
+  radioGroup: {
+    margin: theme.spacing(1, 'auto'),
+  },
+  checkedIcon: {
+    borderRadius: '50%',
+    backgroundColor: '#1E3932',
+    '&:before': {
+      display: 'block',
+      width: 24,
+      height: 24,
+      content: '""',
+    },
+  },
   button: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(3),
     background: '#1E3932',
     color: 'white',
     border: '1px solid #1E3932',
@@ -45,14 +67,18 @@ const Form = () => {
   const classes = useStyles();
   const history = useHistory();
 
-  const [name, setName] = useState('');
-  const handleChange = (event) => {
-    setName(event.target.value);
-    console.log(name);
+  const [formState, setFormState] = useState(EMPTY_FORM);
+  const handleNameChange = (event) => {
+    setFormState({ ...formState, name: event.target.value });
+    console.log(formState.name);
+  };
+  const handleRatingChange = (event) => {
+    setFormState({ ...formState, rating: event.target.value });
+    console.log(formState.rating);
   };
   const handleVote = () => {
-    if (name !== '') {
-      setName('');
+    if (formState.name !== '' && formState.rating !== '') {
+      setFormState(EMPTY_FORM);
       history.push('/voted');
     }
   };
@@ -61,28 +87,62 @@ const Form = () => {
     <Container className={classes.root}>
       <p>If you had good sevice, you can vote for your barrista here!</p>
       <FormControl className={classes.formControl}>
-        <InputLabel shrink id="name-label">
+        {/* <InputLabel shrink id="name-label">
           Name
-        </InputLabel>
+        </InputLabel> */}
+        <FormLabel component="legend" className={classes.radioLabel}>
+          Name
+        </FormLabel>
         <Select
           labelId="name-label"
           id="names"
-          value={name}
-          onChange={handleChange}
+          value={formState.name}
+          onChange={handleNameChange}
           displayEmpty
           className={classes.selectEmpty}>
           <MenuItem value="">
             <em>-- select one --</em>
           </MenuItem>
           {names.map((name) => (
-            <MenuItem value={name.toLowerCase()} className={classes.selection}>
+            <MenuItem value={name.toLowerCase()} key={name} className={classes.selection}>
               {name}
             </MenuItem>
           ))}
         </Select>
+        <FormLabel component="legend" className={classes.radioLabel}>
+          Grade
+        </FormLabel>
+        <RadioGroup
+          aria-label="rating"
+          name="rating"
+          value={formState.rating}
+          onChange={handleRatingChange}
+          className={classes.radioGroup}
+          row>
+          <FormControlLabel
+            value="A"
+            control={<Radio checkedIcon={<span className={classes.checkedIcon} />} />}
+            label="A"
+          />
+          <FormControlLabel
+            value="B"
+            control={<Radio checkedIcon={<span className={classes.checkedIcon} />} />}
+            label="B"
+          />
+          <FormControlLabel
+            value="C"
+            control={<Radio checkedIcon={<span className={classes.checkedIcon} />} />}
+            label="C"
+          />
+          <FormControlLabel
+            value="D"
+            control={<Radio checkedIcon={<span className={classes.checkedIcon} />} />}
+            label="D"
+          />
+        </RadioGroup>
         <Button
           variant="contained"
-          disabled={name === '' ? true : false}
+          disabled={formState.name === '' || formState.rating === '' ? true : false}
           className={classes.button}
           onClick={handleVote}>
           Vote Now!
