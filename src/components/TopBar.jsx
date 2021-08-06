ca69152c815d0224d9587d890504bef8adbd0bf6
ryanp <ryanpanimation@gmail.com>
2021-08-06 02:54:20 -0500

    add logo to top bar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
+import logo from './starbucks_logo.png';
 
 const useStyles = makeStyles((theme) => ({
   nav: {
-    background: '#1E3932',
+    background: '#eeeeee',
     padding: theme.spacing(1.5, 0),
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    height: '45px',
+    width: '45px',
+    margin: theme.spacing(0, 1),
+  },
+  text: {
+    color: '#1E3932',
+    margin: theme.spacing(0, 2),
   },
 }));
 
 const TopBar = () => {
   const classes = useStyles();
+
   return (
     <AppBar position="static" className={classes.nav}>
-      <Typography variant="h4">Starbucks Voting</Typography>
+      <a href="/dashboard">
+        <img src={logo} alt="starbucks logo" className={classes.logo} />
+      </a>
+      <Typography variant="h4" className={classes.text}>
+        Starbucks Voting
+      </Typography>
     </AppBar>
   );
 };
